d3dd491c76b0cbc07832b15a577afaf6f50837ec
satoshi <uglysatoshi@ya.ru>
2023-03-22 22:51:28 +0300

    Сука помогите первую лабу сдать

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,63 @@
 import java.util.Scanner;
+
 public class Main {
-    public static void Hard(double x, double y, double a, double b) {
-        System.out.println("Высокий уровень: ");
-        double K = (Math.sqrt((x + b - a)) + Math.log(y)) / Math.atan(b + a);
-        System.out.println("Функция K = " + K);
-    }
-    public static void Medium(double x, double y) {
-        System.out.println("Средний уровень: ");
-        double L = (0.81 * Math.cos(x)) / Math.log(y) + 2 * Math.pow(x, 3);
-        System.out.println("Функция L = " + L);
-    }
-    public static void Base(double x, double y) {
-        System.out.println("Базовый уровень: ");
-        double M = Math.cos(2*y) + 3.6 * Math.pow(Math.exp(1), x);
-        System.out.println("Функция M = " + M);
-    }
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+    static Scanner in = new Scanner(System.in);
+    public static void Hard() {
+        // Высокий уровень
         System.out.print("Введите x: "); double x = in.nextDouble();
         System.out.print("Введите y: "); double y = in.nextDouble();
-        Base(x, y);
-        System.out.print("Введите y: "); y = in.nextDouble();
-        System.out.print("Введите i: "); double i = in.nextDouble();
-        Medium(i, y);
-        System.out.print("Введите x: "); x = in.nextDouble();
-        System.out.print("Введите y: "); y = in.nextDouble();
         System.out.print("Введите a: "); double a = in.nextDouble();
         System.out.print("Введите b: "); double b = in.nextDouble();
-        Hard(x, y, a, b);
+        // подкоренное выражение не может быть отрицательным
+        while((x + b - a) < 0) {
+            System.out.println("Подкоренное значение меньше нуля, введите значеня x, b, a заново");
+            System.out.print("Введите x: "); x = in.nextDouble();
+            System.out.print("Введите a: "); a = in.nextDouble();
+            System.out.print("Введите b: "); b = in.nextDouble();
+        }
+        // логарифм отрицательный
+        while (Math.log(y) < 0) {
+            System.out.println("Логарифм от отрицательного значения не существует, введите y заново");
+            System.out.print("Введите y: "); y = in.nextDouble();
+        }
+        while(Math.atan(b + a) == 0) {
+            System.out.println("Знаменатель не может быть равен нулю, введите значения b, a заново");
+            System.out.print("Введите a: "); a = in.nextDouble();
+            System.out.print("Введите b: "); b = in.nextDouble();
+        }
+        double K = (Math.sqrt((x + b - a)) + Math.log(y)) / Math.atan(b + a); // функция из базового задания
+        System.out.println("Функция K = " + K); // вывод K
+    }
+    public static void Medium() {
+        // средний уровень
+        System.out.print("Введите x: "); double x = in.nextDouble();
+        System.out.print("Введите y: "); double y = in.nextDouble();
+        // логарифм отрицательный
+        while (Math.log(y) < 0) {
+            System.out.println("Логарифм от отрицательного значения не существует, введите y заново");
+            System.out.print("Введите y: ");
+            y = in.nextDouble();
+            while (Math.log(y) + 2 * Math.pow(x, 3) == 0) {
+                System.out.println("Знаменатель не может быть равен нулю, введите значения y, x заново");
+                System.out.print("Введите y: ");
+                y = in.nextDouble();
+                System.out.print("Введите x: ");
+                x = in.nextDouble();
+            }
+            double L = (0.81 * Math.cos(Math.toDegrees(x))) / Math.log(y) + 2 * Math.pow(x, 3); // фунцкия из среднего уровня
+            System.out.println("Функция L = " + L); // вывод L
+        }
+    }
+    public static void Base() {
+            // Базовый уровень
+            System.out.print("Введите x: "); double x = in.nextDouble();
+            System.out.print("Введите y: "); double y = in.nextDouble();
+        double M = Math.cos(Math.toDegrees(2*y)) + 3.6 * Math.pow(Math.exp(1), x); // функция из высокого уровня
+        System.out.println("Функция M = " + M); // Вывод M
+    }
+    public static void main(String[] args) {
+        Base();
+        Medium();
+        Hard();
     }
 }
